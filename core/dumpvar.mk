3160e9fb158bfb9bcd75e6cba340874f8570f92c
Paul Keith <javelinanddart@bestas.gr>
2017-04-18 07:22:01 -0500

    dumpvar: Dump TARGET_USE_SDCLANG
    * But only do it if SDCLANG is detected locally
    
    Change-Id: I4e9556bfcefc0321351601471b6f5587ff5a42d4
    Signed-off-by: Paul Keith <javelinanddart@bestas.gr>

--- core/dumpvar.mk
+++ core/dumpvar.mk
@@ -12,7 +12,8 @@ print_build_config_vars := \
   TARGET_CPU_VARIANT \
   TARGET_2ND_ARCH \
   TARGET_2ND_ARCH_VARIANT \
-  TARGET_2ND_CPU_VARIANT
+  TARGET_2ND_CPU_VARIANT \
+  TARGET_USE_SDCLANG
 
 ifeq ($(TARGET_BUILD_PDK),true)
 print_build_config_vars += \

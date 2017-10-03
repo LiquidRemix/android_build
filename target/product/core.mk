048f8fa5a569dcd2ba6f4cfc24cdbd01fdc7d21c
liquid0624 <rjbirnesser@gmail.com>
2017-10-03 17:46:39 -0400

    build: cleanup unwanted packages
    Signed-off-by: liquid0624 <rjbirnesser@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -24,7 +24,6 @@ PRODUCT_PACKAGES += \
     BasicDreams \
     BlockedNumberProvider \
     BookmarkProvider \
-    Browser2 \
     BuiltInPrintService \
     Calendar \
     CalendarProvider \
@@ -34,14 +33,11 @@ PRODUCT_PACKAGES += \
     DeskClock \
     DocumentsUI \
     DownloadProviderUi \
-    Email \
-    ExactCalculator \
     ExternalStorageProvider \
     FusedLocation \
     InputDevices \
     KeyChain \
     Keyguard \
-    Launcher2 \
     ManagedProvisioning \
     MtpDocumentsProvider \
     PicoTts \

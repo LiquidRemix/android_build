105c5df2a7b49458a57d81a1c969f4c87068825b
Kshitij Gupta <kshitijgm@gmail.com>
2017-12-17 20:36:21 +0000

    core: Build Settings Dark UI overlay
    Change-Id: I42adb75084bdc54083627d3657104a78a2d19d0f
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>
    Signed-off-by: xyyx <xyyx@mail.ru>
    Signed-off-by: Corey Edwards <ensabahnur16@gmail.com>

--- target/product/core.mk
+++ target/product/core.mk
@@ -49,6 +49,7 @@ PRODUCT_PACKAGES += \
     ProxyHandler \
     QuickSearchBox \
     Settings \
+    SettingsDarkThemeOverlay \
     SharedStorageBackup \
     StorageManager \
     SubstratumService \

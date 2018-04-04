46d5974e8c78319992c528309da270611f54ad22
mydongistiny <jaysonedson@gmail.com>
2018-04-04 00:11:31 -0700

    libart: Change install filter to speed-profile
    also change filter after a dirty flash to verify
    
    Change-Id: Ia5a88008b74daf2a3bb6df1c2f1b140a9c1f98b5
    Signed-off-by: mydongistiny <jaysonedson@gmail.com>

--- target/product/runtime_libart.mk
+++ target/product/runtime_libart.mk
@@ -71,8 +71,8 @@ PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
 
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.first-boot=extract \
-    pm.dexopt.boot=extract \
-    pm.dexopt.install=quicken \
+    pm.dexopt.boot=verify \
+    pm.dexopt.install=speed-profile \
     pm.dexopt.bg-dexopt=speed \
     pm.dexopt.ab-ota=speed \
     pm.dexopt.inactive=verify \
